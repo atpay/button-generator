a1d33a88e0e77d81635b76665b1dc7816fbbc572
Glen <DamnBigMan@gmail.com>
2013-07-25 13:40:59 -0600

    moving all of it around

--- lib/atpay/button/generator.rb
+++ lib/atpay/button/generator.rb
@@ -1,151 +1,47 @@
 module AtPay
   module Button
     class Generator
-      include Validation::Email
-      include Validation::Amount
-
       SOURCES = [
         :cards,
         :emails,
         :members
       ]
 
-      attr_accessor :amount, 
-        :email, 
-        :owner, 
-        :members, 
-        :prospects,
-        :group,
-        :wrapper,
-        :image,
-        :color,
-        :type,
-        :user_data
+      def initialize(session, options)
+        @session = session
 
-      def initialize(options)
-        self.amount = options[:amount]
-        self.owner = options[:owner]
-        self.wrapper = options[:wrapper]
-        self.image = options[:image]
-        self.color = options[:color]
-        self.type = options[:type]
-        self.user_data = options[:user_data]
-
-        @env = options[:env]
-
-        set_targets options[:targets]
-        build_session options[:partner_id], options[:keys]
-
-        @member_map = {}
+        @options = { 
+          :title => "Pay",
+          :type => :payment,
+          :group => nil,
+          :user_data => nil
+        }.update options
       end
 
-      def amount=(v)        
-        unless v.nil? or v.empty?
-          @amount = v.to_s.gsub(/[^0-9\.]/, "").to_f 
-        else
-          @amount = v
-        end
-
-        validate_amount
+      def email
+        @options[:email]
       end
 
-      def email=(v)
-        @email = (v || "").split(/\n|\r|,/).collect(&:strip).select { |e| 
-          !e.strip.blank?
-        }
-
-        validate_email
+      def amount
+        @amount ||= @options[:amount].gsub(/[^0-9\.]/, "").to_f
+      rescue
+        nil
       end
 
-      def user_data=(v)
-        return if v.nil?
-
-        raise Validation::LengthError.new "user_data can't be longer than 2,500 characters, you provided #{v.length} characters" if v.length > 2500
-
-        @user_data = v
+      def token(type, source)
+        @session.security_key(@options.merge({type => source})).email_token
       end
 
-      # Default to payment buttons.
-      def type=(v)
-        @type = v ? v.to_sym : :payment
+      def template
+        @template ||= Template.new(@options[:template])
       end
 
-      def buttons
-        type == :payment ? payment_buttons : validation_buttons
+      def to_html(token)
+        @template.render(:token => t)
       end
 
-      def validation_buttons
-        generator = TokenGenerator.new type: :validation, amount: amount, email: email.join(','), owner: owner, user_data: user_data
-
-        @buttons ||= generator.validation_tokens.collect do |email, token|
-          {
-            email: email,
-            amount: amount,
-            button: member_button(token).to_html
-          }
-        end
-      end
-
-      def payment_buttons
-        build_security_keys
-
-        @buttons ||= mailto_buttons
-      end
-
-      def generate
-        
-      end
-
-
-      private
-
-      def build()
-        
-      end
-
-      def set_targets(targets)
-        (targets.keys & SOURCES).each do |type|
-          instance_variable_set('@' + type.to_s, targets[type])
-        end
-      end
-
-      def build_session(partner_id, keys)
-        @session = AtPay::Session.new({
-          public_key: keys[:public],
-          private_key: keys[:private],
-          partner_id: partner_id,
-          environment: @env
-        })
-      end
-
-      def generate_tokens(source = nil)
-        @tokens = {}
-
-        if source
-          ((source.respond_to?(:each) ? source : [source]) & SOURCES).each do |source|
-            @tokens[source] = tokens_for(source) if instance_variable_get('@' + source.to_s)
-          end
-        else
-          SOURCES.each do |source|
-            @tokens[source] = tokens_for(source) if instance_variable_get('@' + source.to_s)
-          end
-        end
-      end
-
-      def tokens_for(source_type)
-        targets = instance_variable_get("@" + source_type.to_s)
-
-        # There is a horrible line below.  It really should be cleaned up ASAP
-        targets.inject([]) do |collection, target|
-          [
-            target,
-            @session.security_key({
-              amount: amount,
-              source_type.to_s.chop.to_sym => target,
-              group: group
-            }).email_token
-          ]
-        end
+      def build(type, source)
+        to_html token(type, source)
       end
     end
   end
--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -1,86 +1,53 @@
 require 'spec_helper'
 
 describe AtPay::Button::Generator do
-  let(:params) { { amount: '20', targets: {cards: ['383tdjgh37'], emails: ['bob@com.com']}, partner_id: 1, keys: {public: 'bob', private: 'privatebob'}, env: :sandbox } }
-  let(:subject) { AtPay::Button::Generator.new params }
+  let(:session) { mock }
+  let(:options) { {partner_id: 1, public_key: 'bob', private_key: 'privatebob', environment: :sandbox} }
+  let(:subject) { AtPay::Button::Generator.new session, options }
 
   describe "#new" do
-    describe "type" do
-      it "defaults to payment" do
-        subject.type.must_equal :payment
-      end
-
-      it "accepts validation" do
-        gen = AtPay::Button::Generator.new params.merge({type: :validation})
-
-        gen.type.must_equal :validation
-      end
+    it "adds the provided options to the defaults" do
+      subject.instance_eval{ @options }.keys.sort.must_equal [:partner_id, :public_key, :private_key, :environment, :title, :type, :group, :user_data].sort
     end
   end
 
-  describe "#user_data=" do
-    it "doesn't allow more than 2500 characters" do
-      assert_raises(AtPay::Button::Validation::LengthError) { AtPay::Button::Generator.new params.merge({user_data: ('a' * 2501)}) }
-    end
-  end
-
-  describe "#set_targets" do
-    it "assigns the given values to instance variables" do
-      subject.instance_eval { @cards }.must_equal ['383tdjgh37']
-      subject.instance_eval { @emails }.must_equal ['bob@com.com']
-      subject.instance_eval { @members }.must_be :nil?
-    end
-  end
-
-  describe "#build_session" do
-    it "configures the session with the provided parameters" do
-      AtPay::Session.expects(:new).with public_key: 'bob', private_key: 'privatebob', partner_id: 1, environment: :sandbox
-
-      subject
-    end
-  end
-
-  describe "#generate_tokens" do
+  describe "#token" do
     before do
-      @dummy_session = mock
       @dummy_key = mock
-      @dummy_key.stubs(:email_token).returns 'token'
-      AtPay::Session.stubs(:new).returns(@dummy_session)
+      session.expects(:security_key).returns(@dummy_key)
     end
 
-    it "generates tokens for all target types if none specified" do
-      @dummy_session.expects(:security_key).with(any_parameters) { |options| options.has_key? :email }.returns(@dummy_key)
-      @dummy_session.expects(:security_key).with(any_parameters) { |options| options.has_key? :card }.returns(@dummy_key)
+    it "builds an @Pay token" do
+      @dummy_key.expects :email_token
 
-      subject.send :generate_tokens
-
-      subject.instance_eval{ @tokens }.keys.must_include :cards
-      subject.instance_eval{ @tokens }.keys.must_include :emails
-      subject.instance_eval{ @tokens }.keys.wont_include :members
-    end
-
-    it "generates tokens for only the specified target type" do
-      @dummy_session.expects(:security_key).with(any_parameters) { |options| options.has_key? :card }.returns(@dummy_key)
-
-      subject.send(:generate_tokens, :cards)
-
-      subject.tokens.keys.must_equal [:cards]
+      subject.token(:card, '3838383')
     end
   end
 
-  describe "#generate" do
-    it "returns an association of buttons and the source value used to build them" do
+  describe "#template" do
+    it "loads the template if not already loaded" do
+      AtPay::Button::Template.expects(:new)
+
+      subject.template
+    end
+
+    it "does't load the template if it has already been loaded" do
+      subject.template
+      AtPay::Button::Template.expects(:new).never
+
+      subject.template
+    end
+  end
+
+  describe "#to_html" do
+    it "renders the template with the given token" do
       
     end
   end
 
   describe "#build" do
-    before do
-      AtPay::Button::Button.expects(:button).at_least_once.with(:card, /.*/)
-    end
-
-    it "passes the source information to the Button class" do
-      subject.send(:build, :card, 'bob')
+    it "initiates the process and returns the rendered template code" do
+      
     end
   end
 end
