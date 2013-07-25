8d1823e70bf410532981342d0a4117b01fa9ea77
Isaiah <isaiah@atpay.com>
2013-07-25 10:12:02 -0600

    remove atpay.rb and change tom@atpay.com to tom@example.com in cmd line example.

--- lib/atpay/atpay.rb
+++
@@ -1,8 +0,0 @@
-require 'button/validation/amount_validation'
-require 'button/validation/email_validation'
-require 'button/validation/exceptions'
-require 'atpay-client'
-require 'button/template'
-require 'button/generator'
-
-module AtPay; end
