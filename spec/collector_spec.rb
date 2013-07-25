36bebeb48131e69a903d6785dd6775072d824162
Glen <DamnBigMan@gmail.com>
2013-07-25 14:44:22 -0600

    almost done with the refactor I think.

--- spec/collector_spec.rb
+++ spec/collector_spec.rb
@@ -24,14 +24,6 @@ describe AtPay::Button::Collector do
     end
   end
 
-  describe "#set_targets" do
-    it "assigns the given values to instance variables" do
-      subject.instance_eval { @cards }.must_equal ['383tdjgh37']
-      subject.instance_eval { @emails }.must_equal ['bob@com.com']
-      subject.instance_eval { @members }.must_be :nil?
-    end
-  end
-
   describe "#build_session" do
     it "configures the session with the provided parameters" do
       AtPay::Session.expects(:new).with public_key: 'bob', private_key: 'privatebob', partner_id: 1, environment: :sandbox
@@ -40,47 +32,11 @@ describe AtPay::Button::Collector do
     end
   end
 
-  describe "#generate_tokens" do
-    before do
-      @dummy_session = mock
-      @dummy_key = mock
-      @dummy_key.stubs(:email_token).returns 'token'
-      AtPay::Session.stubs(:new).returns(@dummy_session)
-    end
-
-    it "generates tokens for all target types if none specified" do
-      @dummy_session.expects(:security_key).with(any_parameters) { |options| options.has_key? :email }.returns(@dummy_key)
-      @dummy_session.expects(:security_key).with(any_parameters) { |options| options.has_key? :card }.returns(@dummy_key)
-
-      subject.send :generate_tokens
-
-      subject.instance_eval{ @tokens }.keys.must_include :cards
-      subject.instance_eval{ @tokens }.keys.must_include :emails
-      subject.instance_eval{ @tokens }.keys.wont_include :members
-    end
-
-    it "generates tokens for only the specified target type" do
-      @dummy_session.expects(:security_key).with(any_parameters) { |options| options.has_key? :card }.returns(@dummy_key)
-
-      subject.send(:generate_tokens, :cards)
-
-      subject.tokens.keys.must_equal [:cards]
-    end
-  end
-
   describe "#generate" do
     it "returns an association of buttons and the source value used to build them" do
-      
-    end
-  end
+      AtPay::Button::Generator.any_instance.expects(:build).returns('button')
 
-  describe "#build" do
-    before do
-      AtPay::Button::Button.expects(:button).at_least_once.with(:card, /.*/)
-    end
-
-    it "passes the source information to the Button class" do
-      subject.send(:build, :card, 'bob')
+      subject.generate.must_equal [['383tdjgh37', 'button'], ['bob@com.com', 'button']]
     end
   end
 end
