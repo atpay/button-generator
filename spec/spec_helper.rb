2a178857dca1302c22da5fa38a94beacdc8c9025
Frank LaFond <frank@atpay.com>
2013-07-26 12:15:15 -0600

    Improves formatting of output for TeamCity. Is not executed unless running in TC.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,10 @@ require 'minitest/autorun'
 
 require 'mocha/setup'
 
+# See http://www.jetbrains.com/ruby/webhelp/minitest.html
+if ENV["RM_INFO"] || ENV["TEAMCITY_VERSION"]
+  require 'active_support'
+  require 'minitest/reporters'
+  MiniTest::Reporters.use! MiniTest::Reporters::RubyMineReporter.new
+end
 
