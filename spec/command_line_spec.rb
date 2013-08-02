7e4987858b3a7ae578d232dd72f20368866891b7
Glen <DamnBigMan@gmail.com>
2013-08-02 14:41:48 -0600

    Fixed executable name in specs

--- spec/command_line_spec.rb
+++ spec/command_line_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe "atpay-button-generator" do
+describe "atpay_buttons" do
   let(:session) { mock }
   let(:file_info) { 'test_data.txt' }
   let(:public_key) { '06zK82iu9NUUMmDiZsEvoUH25tbIE6R3R+zPnDK8YGQ=' }
   let(:private_key) { 'plBs9X+Zvr65z6iCa0oLNdAEGYZ85Dzf74Qy1yPTris=' }
   let(:options) { {partner_id: 1, public_key: public_key, private_key: private_key, amount: '12', env: :sandbox, user_data: 'aaaaaaaa'} }
-  let(:cli_input) { "bin/atpay-button-generator --amount 5.0 -p #{private_key} -u #{public_key} -r #{options[:partner_id]} -s subject -i imageurl -c color -t title -w false -e sandbox -n #{file_info}" }
+  let(:cli_input) { "bin/atpay_buttons --amount 5.0 -p #{private_key} -u #{public_key} -r #{options[:partner_id]} -s subject -i imageurl -c color -t title -w false -e sandbox -n #{file_info}" }
   let(:cli_output) {`#{cli_input}`}
 
   describe "button creation" do
@@ -20,7 +20,7 @@ describe "atpay-button-generator" do
     end
 
     it "generates valid usage information" do
-      assert_match(/--help, -h:   Show this message/,`bin/atpay-button-generator -h`)
+      assert_match(/--help, -h:   Show this message/,`bin/atpay_buttons -h`)
     end
 
     #it "displays a message if the input file is missing" do
